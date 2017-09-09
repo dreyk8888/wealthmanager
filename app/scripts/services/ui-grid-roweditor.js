28d3d0d55d1d9add0fb838b0401d88387c75aac9
dreyk8888 <dreyk8888@users.noreply.github.com>
2017-09-09 11:10:02 -0400

    Got modal working, but is ugly

--- app/scripts/services/ui-grid-roweditor.js
+++ app/scripts/services/ui-grid-roweditor.js
@@ -1,4 +1,4 @@
-'use strict';
+//'use strict';
 
 /**
  * @ngdoc service
@@ -15,8 +15,37 @@ angular.module('wealthManagerApp')
     function editRow(grid, row) {
        var modalInstance = $uibModal.open({
             templateUrl: 'views/edit-modal.html',
-            controller: ['$uibModalInstance', 'AssetSchema', 'grid', 'row', AssetRowEditCtrl],
-            controllerAs: 'editModal',
+            //controller: AssetRowEditCtrl,
+            //controllerAs: 'editModal',
+            controller: function ($scope, $uibModalInstance, AssetSchema, grid, row) {
+                console.log("Running AssetRowEditCtrl");
+                //var $scope = this;
+                console.log ("Test");
+                $scope.schema = AssetSchema;
+                $scope.entity = angular.copy(row.entity);
+                $scope.form = [
+                    'class',
+                    'name',
+                    'units',
+                    'unitCost',
+                    'amount',
+                    'location',
+                    'date_purchased',
+                    'currency'
+                ];
+
+                $scope.save =  function save() {
+                    console.log ("Saving values");
+                    // Copy row values over
+                    row.entity = angular.extend(row.entity, $scope.entity);
+                    $uibModalInstance.close(row.entity);
+                };
+
+                $scope.cancel = function cancel(){
+                    console.log("Cancelled");
+                    $uibModalInstance.dismiss('cancel');
+                };
+            },
             resolve: {
                 grid: function () { return grid; },
                 row: function () { return row; }

948f99c22f7bc46701e72c3d950a704ed597b2db
dreyk8888 <dreyk8888@users.noreply.github.com>
2017-09-10 15:12:44 -0400

    Put global constants in a single location

---
+++ app/scripts/global-constants.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wealthManagerApp.constant:form-schemas
+ * @description
+ *  * Constants in use in the app
+ */
+
+ angular.module('wealthManagerApp')
+  .constant ('GlobalConstants', {
+        API_URL_LOCAL: 'http://localhost:4000', //local API URL
+        UNDEFSTR: '---',   //empty string representation in app
+        UNDEFNUM: -1,
+        UNDEFDATE: '1/1/1111',
+        //asset classes
+        CASH: 'Cash',
+        EQUITIES: 'Equities',
+        FIXEDINCOME: 'Fixed Income',
+        FIXEDASSETS: 'Fixed Assets',
+        FOREIGNCURR: 'Foreign Currency'
+});

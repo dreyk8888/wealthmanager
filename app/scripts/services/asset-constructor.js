2a27dc54b5a524da57582f7ee317a2065ce83aa2
dreyk8888 <dreyk8888@users.noreply.github.com>
2017-09-10 15:16:16 -0400

    Change asset constructor to initiate objects differently depending on asset class

--- app/scripts/services/asset-constructor.js
+++ app/scripts/services/asset-constructor.js
@@ -9,7 +9,7 @@
  * Also includes functionality to set up Asset objects for posting to API
  */
 angular.module('wealthManagerApp')
-    .service('Asset', function() {
+    .service('Asset', ['GlobalConstants', function(GlobalConstants) {
         //return a clean Asset object
         this.init = function(){
             var asset = {
@@ -41,32 +41,70 @@ angular.module('wealthManagerApp')
         //keep track of _id for API posting purposes
         this.populate = function (asset, _id, assetClass, name, units, unitCost, amount, location, date_purchased, currency){
             asset._id = _id;
-            asset.class = assetClass.toTitleCase();
-            asset.name = name;
-            asset.units = Number(units);
-            asset.unitCost = Number(unitCost);
-            asset.amount = asset.units*asset.unitCost;
-            asset.location = location;
-            asset.date_purchased = date_purchased;
-            asset.currency = currency;
 
+            if (asset.class === GlobalConstants.CASH){
+                asset.class = assetClass;
+                asset.name = name;
+                asset.units = GlobalConstants.UNDEFNUM;
+                asset.unitCost = GlobalConstants.UNDEFNUM;
+                asset.amount = amount;
+                asset.location = GlobalConstants.UNDEFSTR;
+                asset.date_purchased = GlobalConstants.UNDEFDATE;
+                asset.currency = currency;
+            } else if (asset.class === GlobalConstants.FIXEDASSETS || asset.class === GlobalConstants.FOREIGNCURR){
+                asset.class = assetClass;
+                asset.name = name;
+                asset.units = GlobalConstants.UNDEFNUM;
+                asset.unitCost = GlobalConstants.UNDEFNUM;
+                asset.amount = amount;
+                asset.location = location;
+                asset.date_purchased = date_purchased;
+                asset.currency = currency;
+            } else {
+                asset.class = assetClass;
+                asset.name = name;
+                asset.units = Number(units);
+                asset.unitCost = Number(unitCost);
+                asset.amount = asset.units*asset.unitCost;
+                asset.location = location;
+                asset.date_purchased = date_purchased;
+                asset.currency = currency;
+            }
             return asset;
         }
 
         //return a copy of an existing Asset object with amount calculated
+        //for fields that don't apply, set to EMPTY --> '---'
         this.copyAndCalculateAmount = function (obj){
             var asset = this.init();
             asset._id = obj._id;
-            asset.class = obj.class.toTitleCase();
-            asset.name = obj.name;
-            asset.units = Number(obj.units);
-            asset.unitCost = Number(obj.unitCost);
-            asset.amount = asset.units*asset.unitCost;
-            asset.location = obj.location;
-            asset.date_purchased = obj.date_purchased;
-            asset.currency = obj.currency;
-
+            asset.class = obj.class;
+            if (asset.class === GlobalConstants.CASH){
+                asset.name = obj.name;
+                asset.units = GlobalConstants.UNDEFNUM;
+                asset.unitCost = GlobalConstants.UNDEFNUM;
+                asset.amount = obj.amount;
+                asset.location = GlobalConstants.UNDEFSTR;
+                asset.date_purchased = GlobalConstants.UNDEFDATE;
+                asset.currency = obj.currency;
+            } else if (asset.class === GlobalConstants.FIXEDASSETS || asset.class === GlobalConstants.FOREIGNCURR){
+                asset.name = obj.name;
+                asset.units = GlobalConstants.UNDEFNUM;
+                asset.unitCost = GlobalConstants.UNDEFNUM;
+                asset.amount = obj.amount;
+                asset.location = obj.location;
+                asset.date_purchased = obj.date_purchased;
+                asset.currency = obj.currency;
+            } else {
+                asset.name = obj.name;
+                asset.units = Number(obj.units);
+                asset.unitCost = Number(obj.unitCost);
+                asset.amount = asset.units*asset.unitCost;
+                asset.location = obj.location;
+                asset.date_purchased = obj.date_purchased;
+                asset.currency = obj.currency;
+            }
             return asset;
         }
 
-});
+}]);

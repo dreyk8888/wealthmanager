3c88a356ededf71a4370915d6f6a862a5f40eba3
dreyk8888 <dreyk8888@users.noreply.github.com>
2017-09-17 22:41:48 -0400

    Use default port for API

--- app/scripts/global-constants.js
+++ app/scripts/global-constants.js
@@ -9,7 +9,7 @@
 
  angular.module('wealthManagerApp')
   .constant ('GlobalConstants', {
-        API_URL_LOCAL: 'http://localhost:4000', //local API URL
+        API_URL_LOCAL: 'http://localhost:3000', //local API URL
         UNDEFSTR: '---',   //empty string representation in app
         UNDEFNUM: -1,
         UNDEFDATE: '1/1/1111',

cecb03ed65701d3ecc766f2b6bec04fd35a4a112
dreyk8888 <dreyk8888@users.noreply.github.com>
2017-10-07 13:56:33 -0400

    Move mock data into separate service. Add more tests for portfolio calculations

---
+++ test/mock/portfolio-calcs.mock.js
@@ -0,0 +1,141 @@
+//portfolio-calcs.mock.js
+//Mock data for testing functions in portfolio-calcs.js
+angular.module('wealthManagerApp')
+  .service('PortfolioCalcsMockData', function() {
+    this.mockDataEmpty = [];
+    this.mockAssetDataFloat = [
+      {
+        class: 'Equities',
+        amount: 50123
+      },
+      {
+        class: 'Cash',
+        amount: 100002.4556
+      },
+      {
+        class: 'Fixed Assets',
+        amount: 0.455698798
+      },
+      {
+        class: 'Fixed Income',
+        amount: 1.5023e6
+      },
+      {
+        class: 'Equities',
+        amount: 20000
+      },
+      {
+        class: 'Fixed Assets',
+        amount: 450305.10
+      },
+      {
+        class: 'Cash',
+        amount: 25366897.156
+      },
+    ];
+
+    this.mockAssetDataZeroes = [
+      {
+        class: 'Fixed Assets',
+        amount: 0.0
+      },
+      {
+        class: 'Cash',
+        amount: 0
+      },
+      {
+        class: 'Equities',
+        amount: 0
+      },
+      {
+        class: 'Fixed Assets',
+        amount: 0.0000000
+      },
+
+      {
+        class: 'Equities',
+        amount: 0.00000
+      }
+    ];
+
+    this.mockAssetDataNegative = [
+      {
+        class: 'Equities',
+        amount: -23320
+      },
+      {
+        class: 'Equities',
+        amount: -35000
+      },
+      {
+        class: 'Cash',
+        amount: -10000
+      },
+      {
+        class: 'Fixed Income',
+        amount: -5000
+      }
+    ];
+
+    this.mockDebtDataNegatives = [
+      {
+        type: 'Long term',
+        amount: -100002.4556
+      },
+      {
+        type: 'Short term',
+        amount: -5.183465e8
+      },
+      {
+        type: 'Short term',
+        amount: 0.475698798
+      },
+      {
+        type: 'Long term',
+        amount: -1.5023e7
+      }
+    ];
+
+    this.mockDebtDataFloat = [
+      {
+        type: 'Long term',
+        amount: 100002.4556
+      },
+      {
+        type: 'Short term',
+        amount: 5.183465e8
+      },
+      {
+        type: 'Short term',
+        amount: 0.475698798
+      },
+      {
+        type: 'Long term',
+        amount: 1.5023e7
+      }
+    ];
+
+    //test that decimals are rounded to 2 decimal places correctly
+    this.mockDataRoundingUp = [
+      {
+        type: 'Short term',
+        amount: 0.123
+      },
+      {
+        class: 'Long term',
+        amount: 0.126
+      }
+    ];
+
+    this.mockDataRoundingDown = [
+      {
+        type: 'Short term',
+        amount: 0.123
+      },
+      {
+        class: 'Long term',
+        amount: 0.121
+      }
+    ];
+
+});
--- test/spec/services/portfolio-calcs.spec.js
+++ test/spec/services/portfolio-calcs.spec.js
@@ -3,12 +3,17 @@
 describe('Service: PortfolioCalcs', function () {
 
   // load the service's module
-  beforeEach(module('wealthManagerApp'));
+  beforeEach(angular.mock.module('wealthManagerApp'));
 
   // instantiate service
   var PortfolioCalcs;
   beforeEach(inject(function (_PortfolioCalcs_) {
     PortfolioCalcs = _PortfolioCalcs_;
+  }));
+
+  var PortfolioCalcsMock;
+  beforeEach(inject(function(_PortfolioCalcsMockData_){
+    PortfolioCalcsMock = _PortfolioCalcsMockData_;
   }));
 
   // A simple test to verify the Users factory exists
@@ -20,146 +25,95 @@ describe('Service: PortfolioCalcs', function () {
     expect(1+3).toEqual(4);
   });
 
-  var mockDataEmpty = [];
-  var mockAssetDataFloat = [
-    {
-      class: 'Equities',
-      amount: 50123
-    },
-    {
-      class: 'Cash',
-      amount: 100002.4556
-    },
-    {
-      class: 'Fixed Assets',
-      amount: 0.455698798
-    },
-    {
-      class: 'Fixed Income',
-      amount: 1.5023e6
-    },
-    {
-      class: 'Equities',
-      amount: 20000
-    },
-    {
-      class: 'Fixed Assets',
-      amount: 450305.10
-    },
-    {
-      class: 'Cash',
-      amount: 25366897.156
-    },
-  ];
-
-  var mockAssetDataZeroes = [
-    {
-      class: 'Equities',
-      amount: 0
-    },
-    {
-      class: 'Equities',
-      amount: 0.00000
-    }
-  ];
-
-  var mockAssetDataNegative = [
-    {
-      class: 'Equities',
-      amount: -23320
-    },
-    {
-      class: 'Equities',
-      amount: -50000
-    }
-  ];
-
-  var mockDebtDataFloat = [
-    {
-      type: 'Short term',
-      amount: 5.183465e8
-    },
-    {
-      class: 'Long term',
-      amount: 100002.4556
-    },
-    {
-      class: 'Short term',
-      amount: 0.475698798
-    },
-    {
-      class: 'Long term',
-      amount: 1.5023e7
-    }
-  ];
-
-  //test that decimals are rounded to 2 decimal places correctly
-  var mockDataRoundingUp = [
-    {
-      type: 'Short term',
-      amount: 0.123
-    },
-    {
-      class: 'Long term',
-      amount: 0.126
-    }
-  ];
-
-  var mockDataRoundingDown = [
-    {
-      type: 'Short term',
-      amount: 0.123
-    },
-    {
-      class: 'Long term',
-      amount: 0.121
-    }
-  ];
 
   it('totalCalc() should exist', function () {
-    expect(PortfolioCalcs.totalCalc(mockDataEmpty, 'amount')).toBeDefined();
+    expect(PortfolioCalcs.totalCalc(PortfolioCalcsMock.mockDataEmpty, 'amount')).toBeDefined();
   });
 
   it('totalCalc(asset data) with large float and exponentials should calculate the correct total with 2 decimal places (returns a string due to fixed function)', function () {
-    expect(PortfolioCalcs.totalCalc(mockAssetDataFloat, 'amount')).toEqual('27489628.17');
+    expect(PortfolioCalcs.totalCalc(PortfolioCalcsMock.mockAssetDataFloat, 'amount')).toEqual('27489628.17');
   });
 
   it('totalCalc(asset data) with zeroes should return 0.00 (returns a string due to fixed function)', function () {
-    expect(PortfolioCalcs.totalCalc(mockAssetDataZeroes, 'amount')).toEqual('0.00');
+    expect(PortfolioCalcs.totalCalc(PortfolioCalcsMock.mockAssetDataZeroes, 'amount')).toEqual('0.00');
   });
 
   it('totalCalc(asset data) with negatives should return negative number with 2 decimals (returns a string due to fixed function)', function () {
-    expect(PortfolioCalcs.totalCalc(mockAssetDataNegative, 'amount')).toEqual('-73320.00');
+    expect(PortfolioCalcs.totalCalc(PortfolioCalcsMock.mockAssetDataNegative, 'amount')).toEqual('-73320.00');
   });
 
   it('totalCalc(debt data) with large float and exponentials should calculate total with 2 decimal places', function () {
-    expect(PortfolioCalcs.totalCalc(mockDebtDataFloat, 'amount')).toEqual('533469502.93');
+    expect(PortfolioCalcs.totalCalc(PortfolioCalcsMock.mockDebtDataFloat, 'amount')).toEqual('533469502.93');
   });
 
   it('totalCalc(data) if 3rd decimal >= 5, should round up', function () {
-    expect(PortfolioCalcs.totalCalc(mockDataRoundingUp, 'amount')).toEqual('0.25');
+    expect(PortfolioCalcs.totalCalc(PortfolioCalcsMock.mockDataRoundingUp, 'amount')).toEqual('0.25');
   });
 
   it('totalCalc(data) if 3rd decimal >= 5, should round up', function () {
-    expect(PortfolioCalcs.totalCalc(mockDataRoundingDown, 'amount')).toEqual('0.24');
+    expect(PortfolioCalcs.totalCalc(PortfolioCalcsMock.mockDataRoundingDown, 'amount')).toEqual('0.24');
   });
 
   it('perTypeTotalCalc() should exist', function () {
-    expect(PortfolioCalcs.totalCalc(mockDataEmpty, 'amount')).toBeDefined();
+    expect(PortfolioCalcs.totalCalc(PortfolioCalcsMock.mockDataEmpty, 'amount')).toBeDefined();
   });
 
-  var expectedData = [
+
+  it('perTypeTotalPercentCalc() should calculate right total for float and exponentials', function () {
+    expect(PortfolioCalcs.perTypeTotalCalc(PortfolioCalcsMock.mockDataEmpty, 'amount')).toBeDefined();
+  });
+
+  var expectedEmptyArray = [];
+  it('perTypeTotalCalc() should return empty array if given an empty object', function () {
+    expect(PortfolioCalcs.perTypeTotalCalc(PortfolioCalcsMock.mockDataEmpty, 'class', 'amount')).toEqual(expectedEmptyArray);
+  });
+
+  var expectedEmptyZeroes = [
+    {
+      type: 'Fixed Assets',
+      total: 0
+    },
+    {
+      type: 'Cash',
+      total: 0
+    },
+    {
+      type: 'Equities',
+      total: 0
+    }
+  ];
+
+  it('perTypeTotalCalc() should return array of zeroes if given an array of zeroes', function () {
+    expect(PortfolioCalcs.perTypeTotalCalc(PortfolioCalcsMock.mockAssetDataZeroes, 'class', 'amount')).toEqual(expectedEmptyZeroes);
+  });
+
+  var expectedNegativeDebtData = [
+    {
+      type: 'Long term',
+      total: -15123002.4556
+    },
+    {
+       type: 'Short term',
+       total: -518346499.524301202
+    }
+  ];
+  it('perTypeTotalCalc() should handle negative values', function () {
+    expect(PortfolioCalcs.perTypeTotalCalc(PortfolioCalcsMock.mockDebtDataNegatives, 'type', 'amount')).toEqual(expectedNegativeDebtData);
+  });
+
+  //expected order is whichever order it encounters all the types/classes
+  var expectedDataMockAssetDataFloat = [
     {
       type: 'Equities',
       total: 70123
     },
     {
       type: 'Cash',
-      total: 100002.4556
+      total: 25466899.6116
     },
     {
       type: 'Fixed Assets',
-      total: 450305.10
+      total: 450305.555698798
     },
     {
        type: 'Fixed Income',
@@ -167,8 +121,66 @@ describe('Service: PortfolioCalcs', function () {
     }
   ];
 
-  it('perTypeTotalCalc() should calculate right total for float and exponentials', function () {
-    expect(PortfolioCalcs.perTypeTotalCalc(mockAssetDataFloat, 'class', 'amount')).toEqual(expectedData);
+  it('perTypeTotalCalc() should calculate right total for Asset data', function () {
+    expect(PortfolioCalcs.perTypeTotalCalc(PortfolioCalcsMock.mockAssetDataFloat, 'class', 'amount')).toEqual(expectedDataMockAssetDataFloat);
+  });
+
+  var expectedDataMockDebtDataFloat = [
+    {
+      type: 'Long term',
+      total: 15123002.4556
+    },
+    {
+       type: 'Short term',
+       total: 518346500.475698798
+    }
+  ];
+
+  it('perTypeTotalCalc() should calculate right total for debt data', function () {
+    expect(PortfolioCalcs.perTypeTotalCalc(PortfolioCalcsMock.mockDebtDataFloat, 'type', 'amount')).toEqual(expectedDataMockDebtDataFloat);
+  });
+
+  var expectedDataMockDebtDataPercent = [
+    {
+      type: 'Long term',
+      total: 15123002.4556,
+      percentage: '2.83'
+    },
+    {
+       type: 'Short term',
+       total: 518346500.475698798,
+       percentage: '97.17'
+    }
+  ];
+
+  it('perTypeTotalPercentCalc() should calculate right total and percentage for debt data', function () {
+    expect(PortfolioCalcs.perTypeTotalPercentCalc(PortfolioCalcsMock.mockDebtDataFloat, 'type', 'amount')).toEqual(expectedDataMockDebtDataPercent);
+  });
+
+  var expectedDataMockAssetDataPercent = [
+    {
+      type: 'Equities',
+      total: 70123,
+      percentage: '0.26'
+    },
+    {
+      type: 'Cash',
+      total: 25466899.6116,
+      percentage: '92.64'
+    },
+    {
+      type: 'Fixed Assets',
+      total: 450305.555698798,
+      percentage: '1.64'
+    },
+    {
+       type: 'Fixed Income',
+       total: 1.5023e6,
+      percentage: '5.46'
+    }
+  ];
+  it('perTypeTotalPercentCalc() should calculate right total and percentage for asset data', function () {
+    expect(PortfolioCalcs.perTypeTotalPercentCalc(PortfolioCalcsMock.mockAssetDataFloat, 'class', 'amount')).toEqual(expectedDataMockAssetDataPercent);
   });
 
 });

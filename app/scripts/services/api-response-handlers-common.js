e012365b257098df59cb56630c7bf76fadd48826
dreyk8888 <dreyk8888@users.noreply.github.com>
2017-10-10 11:42:09 -0400

    GET failure handler was misnamed

--- app/scripts/services/api-response-handlers-common.js
+++ app/scripts/services/api-response-handlers-common.js
@@ -13,32 +13,40 @@ angular.module('wealthManagerApp')
 
     this.successHandler_GET = function successHandler_GET() {
         console.log ('API error: Successfully to retrieved data!');
+        return this;
     };
-    this.failure_Handler_GET = function failureHandler_GET() {
+    this.failureHandler_GET = function failureHandler_GET() {
         console.log ('API error: Failed to retrieve data!');
+        return this;
     };
     this.successHandler_POST = function successHandler_POST(data) {
         console.log ("API Success: Posted" + data);
+        return this;
     };
 
     this.failureHandler_POST = function failureHandler_POST() {
         console.log ('API Error: Failed to post data!');
+        return this;
     };
 
     this.successHandler_PUT = function successHandler_PUT(data) {
         console.log ("API Success: Updated" + data);
+        return this;
     };
 
     this.failureHandler_PUT = function failureHandler_PUT() {
         console.log ('API Error: Failed to update data!');
+        return this;
     };
 
     this.successHandler_DELETE = function successHandler_DELETE(id) {
         console.log ("API Success: Deleting:" + id);
+        return this;
     };
 
     this.failureHandler_DELETE = function failureHandler_DELETE() {
         console.log ('API Error: Failed to delete data!');
+        return this;
     };
 
 });

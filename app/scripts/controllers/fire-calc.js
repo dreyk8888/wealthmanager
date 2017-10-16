9e63115cf854810381b76ce163e5eb927e2ff7e5
dreyk8888 <dreyk8888@users.noreply.github.com>
2017-10-15 23:41:49 -0400

    Add firecalc page and controller

---
+++ app/scripts/controllers/fire-calc.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/**
+ * @ngdoc function
+ * @name wealthManagerApp.controller:PortfolioEntryCtrl
+ * @description
+ * # FIRECalcCtrl
+ * Controller of the wealthManagerApp Financial independent calculator
+ */
+
+angular.module("wealthManagerApp")
+.controller("FIRECalcCtrl", ["$scope", function ($scope) {
+
+    var vm = this;
+    var vm.annualReturn = 5;
+}]);

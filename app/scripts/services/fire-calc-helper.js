aee2c8c20a8ab5c3d1bdbd8c04cbb7de0f7171e6
dreyk8888 <dreyk8888@users.noreply.github.com>
2017-10-20 15:09:00 -0400

    Change calc function to return a series of values instead of just final net worth

--- app/scripts/services/fire-calc-helper.js
+++ app/scripts/services/fire-calc-helper.js
@@ -15,11 +15,16 @@ angular.module("wealthManagerApp")
     //annualReturn - expected annual return in percentage (eg. 7)
     //incomePerYear - how much is being added/saved each year
     this.netWorthCalc = function (netWorthStart, annualReturn, incomePerYear, incomeGrowth, numberOfYears, compoundMonthly){
+        var netWorthTrend = []; //annual net worth values
         var netWorth = netWorthStart;
         var interest = annualReturn/100 + 1;    //default compound yearly
         var incomeInterest = incomeGrowth/100 + 1;
         var numPeriods = numberOfYears;         //default number of compounding periods is number of years
         var income = incomePerYear;
+
+        if (numberOfYears === 0){
+            netWorthTrend.push(netWorthStart.toFixed(2)); //handle the case where number of periods is 0 and loop never runs
+        }
 
         if (compoundMonthly){
             interest = annualReturn/12/100 + 1; //return per month is monthly interest
@@ -38,16 +43,18 @@ angular.module("wealthManagerApp")
                 if (n % 12 === 0 &&  year > 0){
                     //this period falls on year end, and it is not the first year, so add the income
                     netWorth = netWorth + income * Math.pow(incomeInterest, year - 1);
+                    netWorthTrend.push(netWorth.toFixed(2));
                 }
             }
             else {
                 netWorth = netWorth + income * Math.pow(incomeInterest, n - 1);
+                netWorthTrend.push(netWorth.toFixed(2));
             }
 
             if (DEBUG){ console.log ("interest rate = " + interest + " n = " + n + " income= " + income + " netWorth = " + netWorth ); }
         }
 
-        return netWorth.toFixed(2);
+        return netWorthTrend;
 
     };
 });
--- test/spec/services/fire-calc-helper.spec.js
+++ test/spec/services/fire-calc-helper.spec.js
@@ -26,56 +26,59 @@ describe("Service: FIRECalcHelper", function () {
   });
 
   it("netWorthCalc() should calculate the future value compounded annually without income added correctly", function () {
-    expect(FIRECalcHelper.netWorthCalc(100000, 7, 0, 0, 2, false)).toEqual("114490.00");
+    expect(FIRECalcHelper.netWorthCalc(100000, 7, 0, 0, 2, false)).toEqual(["107000.00","114490.00"]);
   });
 
   it("netWorthCalc() should calculate the future value compounded monthly without income added correctly", function () {
-    expect(FIRECalcHelper.netWorthCalc(100000, 7, 0, 0, 2, true)).toEqual("114980.60");
+    expect(FIRECalcHelper.netWorthCalc(100000, 7, 0, 0, 2, true)).toEqual(["107229.01", "114980.60"]);
   });
 
   it("netWorthCalc() should calculate the future value compounded annually with income added correctly", function () {
-    expect(FIRECalcHelper.netWorthCalc(100000, 7, 40000, 0, 2, false)).toEqual("197290.00");
+    expect(FIRECalcHelper.netWorthCalc(100000, 7, 40000, 0, 2, false)).toEqual(["147000.00","197290.00"]);
   });
 
   it("netWorthCalc() should calculate the future value compounded monthly with income added correctly", function () {
-    expect(FIRECalcHelper.netWorthCalc(100000, 7, 40000, 0, 3, true)).toEqual("252176.40");
+    expect(FIRECalcHelper.netWorthCalc(100000, 7, 40000, 0, 3, true)).toEqual(["147229.01","197872.20","252176.40"]);
   });
 
   it("netWorthCalc() should calculate the future value compounded annually with income and income growth correctly", function () {
-    expect(FIRECalcHelper.netWorthCalc(100000, 7, 40000, 3, 2, false)).toEqual("198490.00");
+    expect(FIRECalcHelper.netWorthCalc(100000, 7, 40000, 3, 2, false)).toEqual(["147000.00","198490.00"]);
   });
 
   it("netWorthCalc() should calculate the future value compounded monthly with income and income growth correctly", function () {
-    expect(FIRECalcHelper.netWorthCalc(100000, 7, 40000, 3, 3, true)).toEqual("255899.15");
+    expect(FIRECalcHelper.netWorthCalc(100000, 7, 40000, 3, 3, true)).toEqual(["147229.01","199072.20","255899.15"]);
   });
 
   it("netWorthCalc() should handle zeroes for net worth", function () {
-    expect(FIRECalcHelper.netWorthCalc(0, 7, 0, 2, 0, false)).toEqual("0.00");
+    expect(FIRECalcHelper.netWorthCalc(0, 7, 0, 2, 0, false)).toEqual(["0.00"]);
   });
 
   it("netWorthCalc() should handle zeroes for annual return", function () {
-    expect(FIRECalcHelper.netWorthCalc(100000, 0, 0, 2, 0, false)).toEqual("100000.00");
+    expect(FIRECalcHelper.netWorthCalc(100000, 0, 0, 2, 0, false)).toEqual(["100000.00"]);
   });
 
   it("netWorthCalc() should handle zeroes", function () {
-    expect(FIRECalcHelper.netWorthCalc(100000, 0, 0, 0, 0, false)).toEqual("100000.00");
+    expect(FIRECalcHelper.netWorthCalc(100000, 0, 0, 0, 0, false)).toEqual(["100000.00"]);
   });
 
   it("netWorthCalc() should handle negative net worth", function () {
-    expect(FIRECalcHelper.netWorthCalc(-100000, 0, 0, 0, 0, false)).toEqual("-100000.00");
+    expect(FIRECalcHelper.netWorthCalc(-100000, 0, 0, 0, 0, false)).toEqual(["-100000.00"]);
   });
 
   it("netWorthCalc() should handle negative net worth with income by ignoring compounding until net worth positive", function () {
-    expect(FIRECalcHelper.netWorthCalc(-100000, 7, 40000, 0, 5, false)).toEqual("105698.00");
+    expect(FIRECalcHelper.netWorthCalc(-100000, 7, 40000, 0, 5, false)).toEqual(["-60000.00","-20000.00","20000.00","61400.00","105698.00"]);
   });
 
   it("netWorthCalc() should handle zero income but non-zero growth", function () {
-    expect(FIRECalcHelper.netWorthCalc(100000, 0, 0, 3, 0, false)).toEqual("100000.00");
+    expect(FIRECalcHelper.netWorthCalc(100000, 0, 0, 3, 0, false)).toEqual(["100000.00"]);
   });
 
   it("netWorthCalc() should handle negative net worth with income AND growth by ignoring compounding until net worth positive", function () {
-    expect(FIRECalcHelper.netWorthCalc(-100000, 7, 40000, 3, 5, false)).toEqual("118849.92");
+    expect(FIRECalcHelper.netWorthCalc(-100000, 7, 40000, 3, 5, false)).toEqual(["-60000.00","-18800.00","23636.00","68999.60","118849.92"]);
   });
 
+  it("netWorthCalc() negative net worth with income AND growth monthly compounding", function () {
+    expect(FIRECalcHelper.netWorthCalc(-100000, 7, 40000, 3, 5, true)).toEqual(["-60000.00","-18800.00","23636.00","69053.73","119065.98"]);
+  });
 
 });

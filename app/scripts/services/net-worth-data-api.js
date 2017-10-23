c8eeeb9279780bab721c9ecc443fb2112f117f8a
dreyk8888 <dreyk8888@users.noreply.github.com>
2017-10-23 11:00:15 -0400

    Add net worth data api service

---
+++ app/scripts/services/net-worth-data-api.js
@@ -0,0 +1,54 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name wealthManagerApp.wealthManagerData
+ * @description
+ * # wealthManagerData
+ * Service in the wealthManagerApp to access API and retrieve historical net worth data
+ */
+angular.module('wealthManagerApp')
+    .service('NetWorthDataAPI', ['$http', 'GlobalConstants', function($http, GlobalConstants) {
+        var apiURL = GlobalConstants.API_URL_LOCAL + '/networthhistory';
+        //can put additional code here
+        this.getData = function(successHandler, failureHandler) {
+            $http.get(apiURL)
+            .then(function(response){
+                successHandler(response);    //data can't be used outside this function
+            }, function(error) {
+                failureHandler(error);
+            });
+            return this;
+        };
+
+        this.deleteData = function(successHandler, failureHandler, id){
+            $http.delete(apiURL + '/' + id)
+            .then(function(response){
+                successHandler(response, id);    //data can't be used outside this function
+                return true;
+            }, function(error) {
+                failureHandler(error);
+            });
+            return this;
+        };
+
+        this.postData = function(successHandler, failureHandler, data){
+            $http.post(apiURL, JSON.stringify(data))
+            .then(function(response){
+                successHandler(response, data);
+            }, function(error){
+                failureHandler(error);
+            });
+            return this;
+        };
+
+        this.updateData = function(successHandler, failureHandler, data, id){
+            $http.put(apiURL + '/' + id, JSON.stringify(data))
+            .then(function(response){
+                successHandler(response, data);
+            }, function(error){
+                failureHandler(error);
+            });
+            return this;
+        };
+}]);

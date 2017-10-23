9e2ce4247c903f7129dcba7a0de5720d98899b16
dreyk8888 <dreyk8888@users.noreply.github.com>
2017-10-23 10:10:45 -0400

    Newline for long function call

--- app/scripts/controllers/fire-calc.js
+++ app/scripts/controllers/fire-calc.js
@@ -7,7 +7,8 @@
  * # FIRECalcCtrl
  * Controller of the wealthManagerApp Financial independent calculator
  */
-
+// use saved values for net worth
+// save net worth trend
 angular.module("wealthManagerApp")
 .controller("FIRECalcCtrl", ["FIRECalcHelper","FIREChartConfig", function (FIRECalcHelper, FIREChartConfig){
 
@@ -16,13 +17,15 @@ angular.module("wealthManagerApp")
     vm.netWorth = 100000;
     vm.incomePerYear = 50000;
     vm.incomeGrowth = 2;
+    vm.expensePerMonth = 2000;
     vm.numberOfYears = 20;
     vm.compoundMonthly = false;
 
     vm.netWorthChartConfig = FIREChartConfig.NetWorthTrendConfig(vm.netWorthData);
 
     vm.calculateNetWorth = function(){
-        vm.netWorthData = FIRECalcHelper.netWorthCalc(vm.netWorth, vm.annualReturn, vm.incomePerYear, vm.incomeGrowth, vm.numberOfYears, vm.compoundMonthly);
+        vm.netWorthData = FIRECalcHelper.netWorthCalc(vm.netWorth, vm.annualReturn, vm.incomePerYear, vm.incomeGrowth,
+            vm.numberOfYears, vm.compoundMonthly);
         vm.futureNetWorth = vm.netWorthData[vm.netWorthData.length - 1];
 
         for (var i=0; i < vm.netWorthData.length; i++){

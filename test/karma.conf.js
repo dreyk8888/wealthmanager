0f3257e2a3d3d6f12c32aac885ed7330ce06f080
dreyk8888 <dreyk8888@users.noreply.github.com>
2017-10-23 17:11:32 -0400

    Add moment.js to handle date times

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -35,6 +35,7 @@ module.exports = function(config) {
       'bower_components/angular-chart.js/dist/angular-chart.js',
       'bower_components/highcharts/highcharts.js',
       'bower_components/highcharts-ng/dist/highcharts-ng.js',
+      'bower_components/moment/moment.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',

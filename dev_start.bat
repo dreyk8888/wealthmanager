7b59cb061299d7ebfe83ff01dff1a2004c8e7d19
dreyk8888 <dreyk8888@users.noreply.github.com>
2017-11-19 12:22:02 -0500

    Update batch file

--- dev_start.bat
+++ dev_start.bat
@@ -7,9 +7,12 @@ c:/mongodb/bin/mongod.exe --dbpath="c:\mongodata\db"
 #run express server
 cd \
 cd web development
-cd WealthManager
 cd wealth-mgr-api
 npm install
 # run the app on Windows
 SET PORT=4000
 npm start
+
+cd ..
+cd WealthManager
+grunt serve

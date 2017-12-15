9be932f201966f2085eebc41c9170eb1c56f94fd
dreyk8888 <dreyk8888@users.noreply.github.com>
2017-12-15 11:44:04 -0500

    Update start command list to use port 3000

--- dev_start.bat
+++ dev_start.bat
@@ -2,11 +2,13 @@
 Have to be bower, since yeoman builds the script/css section automatically based on what's in the bower.json file
 
 #start mongo instance
-cd startup
-start mongo_start.bat
+start c:/mongodb/bin/mongod.exe --dbpath="c:\mongodata\db"
 
 #run express server
-start backend_start.bat
-
-#run front end server
-start frontend_start.bat
+cd \
+cd web development
+cd wealth-mgr-api
+npm install
+# run the app on Windows
+SET PORT=3000
+start npm start
